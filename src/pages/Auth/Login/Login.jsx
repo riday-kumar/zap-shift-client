81a183289640fcacd8be74868b2d43f75a9232c5
riday-kumar <hridoykumarsaha07@gmail.com>
2026-04-14 12:45:28 +0600

    login form added

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Logo from "../../../components/Logo/Logo";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const handleLogIn = (data) => {
+    console.log(data);
+  };
   return (
     <div className="w-[90%] lg:w-[70%] relative">
       <div className="absolute -top-16 lg:-top-[70%] lg:-left-12">
@@ -13,20 +22,38 @@ const Login = () => {
         </h2>
         <p>Login With ZapShift</p>
       </div>
-      <form className="">
+      <form onSubmit={handleSubmit(handleLogIn)}>
         <fieldset className="fieldset w-full">
           <label className="label">Email</label>
           <input
             type="email"
+            name="email"
             className="input w-full bg-white"
             placeholder="Email"
+            {...register("email", {
+              required: "Email is Required",
+              pattern: {
+                value: /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/,
+                message: "Invalid Email Address",
+              },
+            })}
           />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500 font-bold">Email is Required</p>
+          )}
           <label className="label">Password</label>
           <input
             type="password"
+            name="password"
             className="input w-full bg-white"
             placeholder="Password"
+            {...register("password", {
+              required: "Password is required",
+            })}
           />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500 font-bold">Password is required</p>
+          )}
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
